a1e45ede64767cd50ab8ebab14f575adbef0a89b
Max Kaye <m@xk.io>
2024-04-23 19:23:34 +1000

    add imgs ;

--- remote_assets/upload-to-s3.sh
+++ remote_assets/upload-to-s3.sh
@@ -2,6 +2,7 @@
 
 # rclone copy ./audio wasabixert:xert/d++/audio -P --no-check-dest --transfers=32
 # rclone copy ./audio cloudflareassets:xertrov/d++/audio -P --no-check-dest --transfers=64
+rclone copy ./img cloudflareassets:xertrov/d++/img -P --transfers=64
 rclone copy ./audio cloudflareassets:xertrov/d++/audio -P --transfers=64
 # rclone copy ./audio/vl cloudflareassets:xertrov/d++/audio/vl -P --transfers=64
 # rclone copyto ./audio/index.txt cloudflareassets:xertrov/d++/audio/index.txt -P --transfers=64
